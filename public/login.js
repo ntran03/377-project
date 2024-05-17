2bd161ce2c0441807242cdd8c3cf043475f1c6db
abhimet <103293511+abhimet@users.noreply.github.com>
2024-05-17 01:17:48 -0400

    Update login.js

--- public/login.js
+++ public/login.js
@@ -3,6 +3,6 @@ const root_url = 'http://localhost:8888/'
 const login_url = `http://localhost:8888/login`;
 
 function login() {
-  window.location.href = "/login";
+  window.location.href = login_url;
 }
 

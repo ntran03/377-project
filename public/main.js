ea14767350afab37967ecf32358659a3af6c4eff
Nicole Tran <nicoletran1234@gmail.com>
2024-05-18 17:53:31 -0400

    added font change and added tokens to pfp links

--- public/main.js
+++ public/main.js
@@ -25,6 +25,7 @@ function fetchToken() {
 // Function to add the token to all links on the page
 function addTokenToLinks() {
   const links = document.querySelectorAll('a');
+  var profileIcons = document.querySelectorAll('.profile-icon a');
   links.forEach(link => {
       const url = new URL(link.href);
       var access = localStorage.getItem("access_token")
@@ -33,6 +34,15 @@ function addTokenToLinks() {
       // Set the hash portion of the URL
       url.hash = hashString;
       link.href = url.toString();
+  });
+  profileIcons.forEach(link => {
+    const url = new URL(link.href);
+    var access = localStorage.getItem("access_token")
+    var refresh = localStorage.getItem("refresh_token")
+    let hashString = `access_token=${access}&refresh_token=${refresh}`;
+    // Set the hash portion of the URL
+    url.hash = hashString;
+    link.href = url.toString();
   });
 }
 
--- public/profile.js
+++ public/profile.js
@@ -154,6 +154,7 @@ function fetchToken() {
 // Function to add the token to all links on the page
 function addTokenToLinks() {
   const links = document.querySelectorAll('a');
+  var profileIcons = document.querySelectorAll('.profile-icon a');
   links.forEach(link => {
       const url = new URL(link.href);
       var access = localStorage.getItem("access_token")
@@ -162,6 +163,15 @@ function addTokenToLinks() {
       // Set the hash portion of the URL
       url.hash = hashString;
       link.href = url.toString();
+  });
+  profileIcons.forEach(link => {
+    const url = new URL(link.href);
+    var access = localStorage.getItem("access_token")
+    var refresh = localStorage.getItem("refresh_token")
+    let hashString = `access_token=${access}&refresh_token=${refresh}`;
+    // Set the hash portion of the URL
+    url.hash = hashString;
+    link.href = url.toString();
   });
 }
 

5869b6aa81b95ed83a615579887938aa435526ae
Nicole Tran <nicoletran1234@gmail.com>
2024-05-19 18:29:29 -0400

    fetch token

--- public/help.js
+++ public/help.js
@@ -1,4 +1,28 @@
+function getHashParams() {
+  var hashParams = {};
+  var e, r = /([^&;=]+)=?([^&;]*)/g,
+      q = window.location.hash.substring(1);
+  while ( e = r.exec(q)) {
+     hashParams[e[1]] = decodeURIComponent(e[2]);
+  }
+  return hashParams;
+}
+
+
+function fetchToken() {
+  var params = getHashParams()
+  if (params) {
+    console.log(params)
+    localStorage.setItem("access_token", params.access_token)
+    localStorage.setItem("refresh_token", params.refresh_token)
+  }
+  console.log(localStorage.getItem("access_token"));
+}
+
 // Function to add the token to all links on the page
+
+
+
 function addTokenToLinks() {
     const links = document.querySelectorAll('a');
     var profileIcons = document.querySelectorAll('.profile-icon a');

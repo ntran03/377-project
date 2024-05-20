69d5235841e92d4c28c6ad858f6c52b485f43867
Nicole Tran <nicoletran1234@gmail.com>
2024-05-19 22:43:46 -0400

    min seeds

--- public/generate.js
+++ public/generate.js
@@ -1,6 +1,6 @@
 async function fetchTopTracks(token) {
     try {
-        const response = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=5', {
+        const response = await fetch('https://api.spotify.com/v1/me/top/tracks?limit=3', {
             headers: {
                 'Authorization': `Bearer ${token}`
             }
